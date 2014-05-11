3534816f599c976ac676c89f82a9c805d99826f2
adam-jhudatascience <adam.jhudatascience@gmail.com>
2014-05-11 15:15:13 -0600

    Completed figure 2.

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-# This script produces Plot 1 for the first course project of
+# This script produces Plot 2 for the first course project of
 # "Exploratory Data Analysis", the fifth course of the 
 # Johns Hopkins University Data Science specialisation on Coursera.
 
@@ -13,6 +13,8 @@ a1full <- read.table(unz(temp, filename), colClasses="character", header=T, sep=
 a1data <- subset(a1full,Date=="1/2/2007" | Date=="2/2/2007")
 rm(a1full)
 unlink(temp)
-# Now make the histogram.
-hist(as.numeric(a1data$Global_active_power),col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
+# Now make the plot.
+a1data$datetime <- as.POSIXct(mapply(paste,a1data$Date,a1data$Time), format="%d/%m/%Y %H:%M:%S")
+plot(a1data$datetime, a1data$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)")
+lines(a1data$datetime, a1data$Global_active_power)
